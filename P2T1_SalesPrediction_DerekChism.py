464f17dd9c2069d1c2cb47d3b48b3f2c41012823
chismd <43526014+chismd@users.noreply.github.com>
2018-09-23 21:39:42 -0400

    Add files via upload
    first submission

---
+++ P2T1_SalesPrediction_DerekChism.py
@@ -0,0 +1,10 @@
+#Get The Projected Sales
+#9-23-18
+#CTI-110 P2T1 - Sales Prediction
+#Derek Chism
+
+total_sales = float( input("Please enter the Projected Amount of Total Sales: "))
+
+profit = (total_sales * 0.23)
+
+print ( "Your profit will be $" +str(profit))
---
+++ P2T1_SalesPrediction_DerekChism2.py
@@ -0,0 +1,7 @@
+Python 2.7.12 (v2.7.12:d33e0cf91556, Jun 27 2016, 15:19:22) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+========== RESTART: C:/Python27/P2T1_SalesPrediction_DerekChism.py ==========
+Please enter the Projected Amount of Total Sales: 2000
+Your profit will be $460.0
+>>> 
